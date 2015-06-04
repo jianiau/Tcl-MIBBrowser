238aafe7e51f5fada78d37ba342f4038e2000a88
Jimmy Huang <jianiau.huang@gmail.com>
2015-06-04 11:03:59 +0800

    Add GPL Notice

--- main.tcl
+++ main.tcl
@@ -1,4 +1,20 @@
 #!/usr/bin/wish
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
 
 set appPath [file normalize [info script]]
 if {[file type $appPath] == "link"} {set appPath [file readlink $appPath]}
@@ -26,16 +42,16 @@ package require inifile
 package require bigint
 package require sha1
 package require ip
-
+package require keynav
+package require ttk::dialog
+package require ttk::icons
 
 
 namespace eval ::snmp {}
 namespace eval ::macro {}
 source [file join $appPath proc PBKDF2.tcl]
 source [file join $appPath proc MIB_browser_proc.tcl]
-source [file join $appPath proc dialog.tcl]
-source [file join $appPath proc icons.tcl]
-source [file join $appPath proc keynav.tcl]
+
 
 
 # set init value
--- proc/PBKDF2.tcl
+++ proc/PBKDF2.tcl
@@ -2,7 +2,23 @@
 # modify from http://wiki.tcl.tk/37331
 # 1. change sha2 to sha1
 # 2. support tcl8.4
- 
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
 namespace eval ::pbkdf2 {}
 
 proc ::pbkdf2::pbkdf2 {password salt count dklen} {
